cab0473b78d6b69cc7e4931027e27fc35ea18f86
Philippe Serhal <philippe.serhal@gmail.com>
2025-01-03 11:12:03 -0500

    docs: clarify comment

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   server: {
     compatibilityDate: "2024-09-24",
     // This is the default given the above ^ but I'm setting it explicitly for clarity.
-    // Setting this to "netlify-legacy" fixes the error.
+    // Setting this to "netlify-legacy" (or "netlify-edge") fixes the error.
     preset: "netlify",
   },
 });
